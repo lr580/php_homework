e0e58dcaa74b61af6c6ca6653aa6303f370afa5c
lr580 <2277379149@qq.com>
2022-06-11 16:14:33 +0800

    修复了分类无法分页的bugs

--- application/index/controller/Showflower.php
+++ application/index/controller/Showflower.php
@@ -10,26 +10,38 @@ class Showflower extends Controller
     public function clsflower()
     {
         $pname = input('get.pname');
+        $pvalue = input('get.pvalue');
+        $pvalue1 = input('get.pvalue1');
+        $pvalue2 = input('get.pvalue2');
         $handler = Db::table($this::$dbname);
-        if (substr($pname, 0, 6) == 'fclass' || $pname == 'tejia') {
-            $pvalue = input('get.pvalue');
-            $handler->where($pname, $pvalue);
-        } else {
-            $pvalue1 = input('get.pvalue1');
-            $pvalue2 = input('get.pvalue2');
-            $handler->where($pname, 'egt', $pvalue1)->where($pname, 'elt', $pvalue2);
-            // return $pname.$pvalue1.$pvalue2;
+        if (!$pname) {
+            $pname = session('pname');
+            $pvalue = session('pvalue');
+            $pvalue1 = session('pvalue1');
+            $pvalue2 = session('pvalue2');
         }
-        // $data = $handler->order('SelledNum', 'desc');
-        // $this->assign('flowers', $data);
-        // $this->assign('page', null);
-        // return $this->fetch('index/showflower');
+        session('pvalue', $pvalue);
+        session('pname', $pname);
+        session('pvalue1', $pvalue1);
+        session('pvalue2', $pvalue2);
+        if (substr($pname, 0, 6) == 'fclass' || $pname == 'tejia') {
+            $handler->where($pname, $pvalue);
+        } else if ($pname) {
+            $handler->where($pname, 'egt', $pvalue1)->where($pname, 'elt', $pvalue2);
+        }
+        $data = $handler->order('SelledNum', 'desc')->paginate(5);
 
-        $data = $handler->order('SelledNum', 'desc')->paginate(114514);
         $this->assign('flowers', $data);
         $page = $data->render();
         $this->assign('page', $page);
         return $this->fetch('index/showflower');
-        /*存在bugs：翻页后不再有分类，所以删除了分页功能 */
+
+        // $nowpage = input('get.page');
+        // if (!$nowpage) {
+        //     $nowpage = 1;
+        // }
+        // if ($nowpage) {
+        //     $handler->limit(5 * ($nowpage - 1));
+        // }
     }
 }
