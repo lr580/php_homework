93874869c74743d9c4c5eafca4cda2499dffdb79
Xiao_Dcs <xiao_dcs@outlook.com>
2022-06-13 01:45:41 +0800

    修改后台主页，鲜花修改，新增，账户信息修改

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,7 +1,11 @@
 <?php
 namespace app\admin\controller;
 
+use app\admin\model\Peisong;
+// use app\admin\model\Showorder;
+use app\index\model\Myorder;
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
@@ -10,6 +14,24 @@ class Index extends Controller
             if(empty(session('username'))){
                 $this->error('请先登录!','adminlogin/login');
             }
-            return $this->fetch('adminindex');   
+            // $fclass = Db::table('flower')->distinct('true')->field('fclass')->select();
+            // $this->assign('fclasses', $fclass);
+            
+            $flowernum=Db::table('flower')->count();
+            $this->assign('flowernum', $flowernum);
+
+            $shoplistnum=Db::table('shoplist')->count();
+            $this->assign('shoplistnum', $shoplistnum);
+
+            $ordernum=Db::table('showorder')->count();
+            $this->assign('ordernum', $ordernum);
+
+            $membernum=Db::table('tb_member')->count();
+            $this->assign('membernum', $membernum);
+
+            $addnum=Db::table('tb_customer')->count();
+            $this->assign('addnum', $addnum);
+            
+            return $this->fetch('adminindex');
     }
 }
